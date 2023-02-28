3b1246adae1383d58e6baacb72011bdd915f9f7d
jashwanth07 <40638709+jashwanth07@users.noreply.github.com>
2023-02-28 20:00:01 +0530

    updated json format

--- src/components/ViewAllBuses.js
+++ src/components/ViewAllBuses.js
@@ -62,8 +62,7 @@ function ViewAllBuses() {
                 </tbody>
             </Table>
 
-
-
+                   
         </Container>
     )
 }
--- src/components/ViewSeats.js
+++ src/components/ViewSeats.js
@@ -1,25 +1,45 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
 const ViewSeats = () => {
+
+    const [seatCol1, setSeats] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+   
+    
+    // const arr = [1,2,3,4,5,6,7,8,9,10];
+    // const sub = (e)=>{
+    //     e.preventDefault();
+    //     // 
+    //     setSeats(seats);
+
+    // }
+    // 
+
+
     return (
         <div className='container'>
             <div className='row justify-content-center mt-5'>
                 <div className='card shadow bg-light border-dark seatsCard'>
-                <div className='col-lg-12 col-md-12 col-sm-12'>
-                        <div className='col-lg-6 col-md-6'>
-                            <input type='checkbox' className='form-check-input' value='1'/>
-                            <input type='checkbox' className='form-check-input' value='1'/>
-                            <input type='checkbox' className='form-check-input' value='1'/>
-                            <input type='checkbox' className='form-check-input' value='1'/>
-                            <input type='checkbox' className='form-check-input' value='1'/>
-                            <input type='checkbox' className='form-check-input' value='1'/>
-                            <input type='checkbox' className='form-check-input' value='1'/>
-                            
+                    <div className='col-lg-12 col-md-12 col-sm-12'>
+                        <div className='col-lg-6 col-md-6 mt-4'>
+                            <div className='col-lg-3 col-md-3'>
+                                {
+                                    seatCol1.map((e, i) =>
+                                        <input type='checkbox' className='form-check-input p-2' key={i} value={e}></input>
+                                    )
+
+                                }
+                            </div>
+                            {
+
+
+
+                            }
+
                         </div>
                         <div className='col-lg-6 col-md-6'>
-                            
                         </div>
-                </div>
+                    </div>
                 </div>
             </div>
         </div>
